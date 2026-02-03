9dc1f93954472147a37780602925b94f4e85dd60
rewind.dll <halcyteexe@gmail.com>
2026-02-03 14:12:52 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'rewind.dll'
 description 'Starter Pack System with Discord Booster Integration'
-version '1.0.0'
+version '1.1.0'
 
 shared_scripts {
     '@es_extended/imports.lua',
